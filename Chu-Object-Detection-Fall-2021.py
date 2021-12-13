f894d50824bdaf1dee9be7fb978c41abdd3016fa
S. Chu <76671950+schu-lab@users.noreply.github.com>
2021-12-12 20:06:11 -0800

    First Commit
    Parent Script: 'Chu-Object-Detection-Fall-2021.py'
    Dependencies: All other files
    TFlite Model produced from Model Maker Object Detection for Android Figure

---
+++ Chu-Object-Detection-Fall-2021.py
@@ -0,0 +1,144 @@
+######## Object Detection using TensorFlow and OpenCV #########
+#
+# Author: Simon Chu
+# Last Updated: 12/12/21
+# Revision: X6
+# Description: Object Detection using Tensor Flow and Open CV.
+# # Refereces:
+# (1) https://www.tensorflow.com - Examples - detect.py
+# (2) Model Maker Object Detection for Android Figure - Images and Pascal VOC
+#     (.jpg + .xml formats)
+# (3) labelImg - Used to classify image with classifier (.xml)
+# (4) https://www.digikey.com/en/maker/projects/
+# how-to-perform-object-detection-with-tensorflow-lite-on-raspberry-pi
+# /b929e1519c7c43d5b2c6f89984883588
+
+"""Main script to run the object detection routine."""
+import argparse
+import sys
+import time
+
+import cv2
+from object_detector import ObjectDetector
+from object_detector import ObjectDetectorOptions
+import utils
+
+
+def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
+        enable_edgetpu: bool) -> None:
+  """Continuously run inference on images acquired from the camera.
+
+  Args:
+    model: Name of the TFLite object detection model.
+    camera_id: The camera id to be passed to OpenCV.
+    width: The width of the frame captured from the camera.
+    height: The height of the frame captured from the camera.
+    num_threads: The number of CPU threads to run the model.
+    enable_edgetpu: True/False whether the model is a EdgeTPU model.
+  """
+
+  # Variables to calculate FPS
+  counter, fps = 0, 0
+  start_time = time.time()
+
+  # Start capturing video input from the camera
+  cap = cv2.VideoCapture(camera_id)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+  # Visualization parameters
+  row_size = 20 
+  left_margin = 24 
+  text_color = (0, 0, 255) 
+  font_size = 1
+  font_thickness = 1
+  fps_avg_frame_count = 10
+
+  # Initialize the object detection model
+  options = ObjectDetectorOptions(
+      num_threads=num_threads,
+      score_threshold=0.3,
+      max_results=3,
+      enable_edgetpu=enable_edgetpu)
+  detector = ObjectDetector(model_path=model, options=options)
+
+  # Continuously capture images from the camera and run inference
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      sys.exit(
+          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+      )
+
+    counter += 1
+    image = cv2.flip(image, 1)
+
+    # Run object detection estimation using the model.
+    detections = detector.detect(image)
+
+    # Draw keypoints and edges on input image
+    image = utils.visualize(image, detections)
+
+    # Calculate the FPS
+    if counter % fps_avg_frame_count == 0:
+      end_time = time.time()
+      fps = fps_avg_frame_count / (end_time - start_time)
+      start_time = time.time()
+
+    # Show the FPS
+    fps_text = 'FPS = {:.1f}'.format(fps)
+    text_location = (left_margin, row_size)
+    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                font_size, text_color, font_thickness)
+
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+      break
+    cv2.imshow('object_detector', image)
+
+  cap.release()
+  cv2.destroyAllWindows()
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model',
+      help='Path of the object detection model.',
+      required=False,
+      default='efficientdet_lite0.tflite')
+  parser.add_argument(
+      '--cameraId', help='Id of camera.', required=False, type=int, default=0)
+  parser.add_argument(
+      '--frameWidth',
+      help='Width of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=640)
+  parser.add_argument(
+      '--frameHeight',
+      help='Height of frame to capture from camera.',
+      required=False,
+      type=int,
+      default=480)
+  parser.add_argument(
+      '--numThreads',
+      help='Number of CPU threads to run the model.',
+      required=False,
+      type=int,
+      default=4)
+  parser.add_argument(
+      '--enableEdgeTPU',
+      help='Whether to run the model on EdgeTPU.',
+      action='store_true',
+      required=False,
+      default=False)
+  args = parser.parse_args()
+
+  run(args.model, int(args.cameraId), args.frameWidth, args.frameHeight,
+      int(args.numThreads), bool(args.enableEdgeTPU))
+
+
+if __name__ == '__main__':
+  main()
